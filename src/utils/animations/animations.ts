71290b73840d2323a93b4d482dbe675deb9e1615
thisisaarush <tanwaraarush007@gmail.com>
2023-01-28 21:53:31 +0530

    modified animations

--- src/utils/animations/animations.ts
+++ src/utils/animations/animations.ts
@@ -8,10 +8,10 @@ export const SlideDownAnimation = {
 
 export const CarouselAnimation = {
   initial: (direction: number) => {
-    return { x: direction > 0 ? "100%" : "-100%" };
+    return { x: direction > 0 ? "100%" : "-100%", opacity: 0 };
   },
-  animate: { x: 0 },
+  animate: { x: 0, opacity: 1 },
   exit: (direction: number) => {
-    return { x: direction < 0 ? "100%" : "-100%" };
+    return { x: direction < 0 ? "100%" : "-100%", opacity: 0 };
   },
 };
