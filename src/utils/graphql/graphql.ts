e42bd7450f12c6cb05ed065f9f2c000914045889
Aarush Tanwar <tanwaraarush007@gmail.com>
2023-07-29 17:29:41 +0530

    Updated server from railway to render

--- src/utils/graphql/graphql.ts
+++ src/utils/graphql/graphql.ts
@@ -10,7 +10,7 @@ export const client = new ApolloClient({
   link: createHttpLink({
     uri:
       process.env.NODE_ENV === "production"
-        ? "https://shoezstore.up.railway.app/" //"https://shoez-store-backend.onrender.com" Disabling render Server due to server issues
+        ? "https://shoez-store-backend.onrender.com" //"https://shoezstore.up.railway.app/" Disabling railway Server due to free tier removal
         : "http://localhost:4000/",
     credentials: "same-origin",
   }),
